e027e4ffa46080197e2aec93a8da84ce00737d04
Nyasha Shawn Majoto <shawnjots@gmail.com>
2024-08-17 14:55:06 +0200

    feat: Added base eventArgs

---
+++ ElevatorChallenge.Events/ElevatorEventArgs.cs
@@ -0,0 +1,16 @@
+﻿namespace ElevatorChallenge.Events
+{
+	public class ElevatorEventArgs
+	{
+		public int ElevatorId { get; set; }
+		public Status NewStatus { get; set; }
+		public int CurrentFloor { get; set; }
+
+		public enum Status
+		{
+			Moving,
+			Stationary,
+			DoorsOpen
+		}
+	}
+}
---
+++ ElevatorChallenge.Events/PassengerEventArgs.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorChallenge.Events
+{
+	public class PassengerEventArgs
+	{
+		public int ElevatorId { get; set; }
+		public int CurrentFloor { get; set; }
+		public int PassengerCount { get; set; }
+		public Status PassengerStatus { get; set; }
+
+		public enum Status
+		{
+			AddedToQueue,
+			BoardedElevator,
+			DepartedElevator,
+			RequestFailed
+		}
+	}
+}
