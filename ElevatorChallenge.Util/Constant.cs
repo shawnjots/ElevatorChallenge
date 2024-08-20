7b5f3f47b6735dffd086733f87bcc3168be6c55e
Nyasha Shawn Majoto <shawnjots@gmail.com>
2024-08-20 22:15:08 +0200

    feat: Added event subscription, consts

---
+++ ElevatorChallenge.Util/Constant.cs
@@ -0,0 +1,20 @@
+﻿namespace ElevatorChallenge.Util
+{
+	public static class Constant
+	{
+		public static int MinFloor = 1;
+		public static int MaxFloor = 10;
+		public static double LoadSensitivity { get; set; } = 0.3;
+		public static double DensityWeight { get; set; } = 0.3;
+		public static int MaxElevators = 4;
+		public static int MaxPassengers = 8;
+		public static int MaxWaitPeriod { get; set; } = 60;
+		public static int PassengerOptimizationLimit { get; set; }
+		public static double WaitFactor { get; set; } = 0.5;
+		public static byte DensityLimit { get; set; } = 8;
+		public static double DensityFactor { get; set; } = 0.4;
+		public static int WaitTimeFactor { get; set; } = 2;
+		public static int ProximityFactor { get; set; } = 2;
+
+	}
+}
--- EventChallenge.Services/ElevatorService.cs
+++ EventChallenge.Services/ElevatorService.cs
@@ -1,7 +1,69 @@
-﻿namespace EventChallenge.Services
-{
-	public class ElevatorService
-	{
+﻿using AutoMapper;
+using ElevatorChallenge.DTOs.Passenger;
+using ElevatorChallenge.Events;
+using ElevatorChallenge.Models;
+using ElevatorChallenge.Util;
+using EventChallenge.Services.Interfaces;
+using System.Collections.Concurrent;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
 
+namespace EventChallenge.Services
+{
+	public class ElevatorService : IElevatorService
+	{
+		private readonly IMapper? _mapper;
+		private readonly object _lock = new();
+		private readonly ConcurrentBag<Elevator>? _elevators;
+		private readonly ConcurrentDictionary<int, Floor>? _floors;
+		private readonly ConcurrentBag<Passenger>? _passengers;
+		private readonly ConcurrentQueue<int>? _requestCount;
+
+
+		public ElevatorService(IMapper mapper)
+		{
+			_requestCount = new ConcurrentQueue<int>();
+			_elevators = new ConcurrentBag<Elevator>();
+			_floors = new ConcurrentDictionary<int, Floor>();
+
+			_mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+
+			EnableElevators();
+			EnableFloors();
+
+			PassengerEvents.Subscribe(async passengerArgs =>
+			{
+				
+			});
+		}
+
+		private readonly Subject<PassengerEventArgs> _passengerStatusSubject = new();
+		public IObservable<PassengerEventArgs> PassengerEvents => _passengerStatusSubject.AsObservable();
+		
+		private readonly Subject<ElevatorEventArgs> _elevatorStatusSubject = new();
+		public IObservable<ElevatorEventArgs> ElevatorEvents => _elevatorStatusSubject.AsObservable();
+
+		public void QueuePassenger(int floor, PassengerDTO passengerDTO)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void EnableFloors()
+		{
+			for (int i = Constant.MinFloor; i <= Constant.MaxFloor; i++)
+			{
+				_floors[i] = new Floor(i);
+			}
+		}
+
+		private void EnableElevators()
+		{
+			for (int i = 1; i <= Constant.MaxElevators; i++)
+			{
+				_elevators.Add(new Elevator(Constant.MaxPassengers));
+			}
+		}
+
+		
 	}
 }
